d606af14a049ab548e872c785d6b9bbdbdffd572
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2022-03-07 16:36:03 +0530

    Front home and login

---
+++ src/app/Components/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  coll:any = document.getElementsByClassName("collapsible");
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    for (let i = 0; i < this.coll.length; i++) {
+      this.coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    } 
+  }
+  
+  
+}
